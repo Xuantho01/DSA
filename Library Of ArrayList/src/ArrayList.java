d8c50ef5b3cc115ebc9874b73122d9d4fdd3d4f4
Xuantho01 <hoxuantho95@gmail.com>
2020-02-27 09:38:11 +0700

    create library of arrayList

--- Library Of ArrayList/src/ArrayList.java
+++ Library Of ArrayList/src/ArrayList.java
@@ -1,54 +1,58 @@
-public class MyList<E>{
-   private int size = 0;
+
+//import java.util.ArrayList;
+
+public class ArrayList<E> {
+   private int Size = 0;
    static final int DEFAULT_CAPACITY = 4;
    private Object[] elements;
 
-   public MyList() {
+   public ArrayList() {
       elements = new Object[DEFAULT_CAPACITY];
    }
+
    private void ensureCapa(){
-      if (size >= elements.length){
-          Object[] newElements = new Object[size*2 +1];
-          System.arraycopy(elements,0,newElements,0,size);
+      if (Size >= elements.length){
+          Object[] newElements = new Object[Size *2 +1];
+          System.arraycopy(elements,0,newElements,0, Size);
           elements = newElements;
       }
    }
-
+   public int getSize(){
+     return elements.length;
+   }
    public void add(int index,E data){
       ensureCapa();
-      for (int i = size - 1; i >= index; i--) {
+      for (int i = Size - 1; i >= index; i--) {
          elements[i + 1] = elements[i];
       }
       elements[index] = data;
-      size++;
+      Size++;
    }
    public void checkIndex(int index){
-      if(index < 0 || index >= size)
+      if(index < 0 || index >= Size)
          throw new IndexOutOfBoundsException("index " +index+" out of bounds");
    }
    public void remove(int index){
       checkIndex(index);
        // Object listAfterRemove = elements[index];
-      for (int i = index; i < size - 1; i++){
+      for (int i = index; i < Size - 1; i++){
          elements[i] = elements[i+1];
       }
-      elements[size - 1] = null;
-      size--;
+      elements[Size - 1] = null;
+      Size--;
    }
    public void trimToSize() {
 
-      if (size != elements.length) {
-         Object[] newData = new Object[size];
-         System.arraycopy(elements, 0, newData, 0, size);
+      if (Size != elements.length) {
+         Object[] newData = new Object[Size];
+         System.arraycopy(elements, 0, newData, 0, Size);
          elements = newData;
       } // If size == capacity, no need to trim
    }
-//   public Arrays ketQua(){
-//      return elements
-//   }
    public void printList() {
      for (int i = 0; i < elements.length; i++){
         System.out.print(elements[i] + "  ");
      }
    }
+
 }
--- Library Of ArrayList/src/testMyarrayList.java
+++ Library Of ArrayList/src/testMyarrayList.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList;
 public class testMyarrayList {
     public static void main(String[] args) {
-        MyList<Integer> myList = new MyList<>();
+        ArrayList<Integer> myList = new ArrayList<>();
         myList.add(0,1);
         myList.add(1,2);
         myList.add(2,3);
@@ -8,6 +9,10 @@ public class testMyarrayList {
 
         myList.remove(2);
         myList.trimToSize();
-        myList.printList();
+
+        myList.add(1212);
+        System.out.println(myList.get(0));
+//        System.out.println("\nSize of array: "+myList.getSize());;
+//    }
     }
 }
